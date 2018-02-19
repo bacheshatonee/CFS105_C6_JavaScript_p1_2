7be4410e7b6d3b5d5ded386ebd457f83f4985b75
bacheshatonee <chriskula@gmail.com>
2018-02-18 23:46:03 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var myFavoriteNumber = 42;
+var myFavoritePet = Howard;
+var rainIsLovely = true;
+
+var numberOne = 24;
+var numberTwo = 57;
+
+var addNumbers = numberOne + numberTwo;
+var divideNumbers = 24 / 57;
